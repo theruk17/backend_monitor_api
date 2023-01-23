ae24344748eb0203c7e52d0302f96b94549a0f21
theruk17 <therukza@hotmail.com>
2023-01-23 22:49:27 +0700

    firstcommit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/monitor' , (req, res) => {
+  connection.query(
+    'SELECT * FROM pd_monitor',
+    function(err, results, fields) {
+      res.send(results)
+    }
+  )
+})
+
+app.listen(process.env.PORT || 3000)
