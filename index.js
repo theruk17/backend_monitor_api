100fafd97217ac1013d75f090203eb42d64f4058
theruk17 <therukza@hotmail.com>
2023-01-27 22:56:40 +0700

    update_edit1

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2')
 require('dotenv').config()
 const app = express()
 
-app.use(express.json)
 app.use(cors())
+app.use(express.json())
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
@@ -31,13 +31,12 @@ app.get('/admin_data' , (req, res) => {
   )
 })
 
-app.put('/edit/:id' , (req, res) => {
+app.put('/edit' , (req, res) => {
   const { id} = req.params.id
-  const { brand, model, size, hz, panel, resolution, price_srp, price} = req.body
   connection.query(
-    `UPDATE pd_monitor SET mnt_group = ?, mnt_brand = ?, mnt_model = ?, mnt_size = ?, mnt_refresh_rate = ?, 
-    mnt_panel = ?, mnt_resolution = ?, mnt_price_srp = ?, mnt_price_w_com = ? WHERE mnt_id = ?`,
-    [brand, model, size, hz, panel, resolution,price_srp, price, id], (err, result) => {
+    `UPDATE pd_monitor SET 'mnt_group' = ?, 'mnt_brand' = ?, 'mnt_model' = ?, 'mnt_size' = ?, 'mnt_refresh_rate' = ?, 
+    'mnt_panel' = ?, 'mnt_resolution' = ?, mnt_price_srp = ?, mnt_price_w_com = ? WHERE mnt_id = ?`,
+    [req.body.brand, req.body.model, req.body.size, req.body.hz, req.body.panel, req.body.resolution,req.body.price_srp, req.body.price, id], (err, result) => {
       if(err) throw err
       res.send("Data updated")
     }
