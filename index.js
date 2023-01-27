990970b1c2ecf841e4b2d856fc120d26b6686747
theruk17 <therukza@hotmail.com>
2023-01-27 23:01:40 +0700

    update_cc

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/admin_data' , (req, res) => {
   )
 })
 
-app.put('/edit/:id' , (req, res) => {
+app.put('/edit' , (req, res) => {
   const { id} = req.params.id
   connection.query(
     `UPDATE pd_monitor SET 'mnt_group' = ?, 'mnt_brand' = ?, 'mnt_model' = ?, 'mnt_size' = ?, 'mnt_refresh_rate' = ?, 
