67f70d7c892897d7c212ddbf2fa8c270a549afde
theruk17 <therukza@hotmail.com>
2023-01-29 20:29:02 +0700

    update290123

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ app.get('/edit_data/:id' , (req, res) => {
 
 app.put('/edit/:id' , (req, res) => {
   const { id }  = req.params
-  const { group, brand, model, size, hz, panel, resolution, price_srp, price } = req.body
+  const { group, brand, model, size, hz, panel, resolution, price_srp, price_w_com, } = req.body
   connection.query(
     `UPDATE pd_monitor SET mnt_group = ?, mnt_brand = ?, mnt_model = ?, mnt_size = ?, mnt_refresh_rate = ?, 
     mnt_panel = ?, mnt_resolution = ?, mnt_price_srp = ?, mnt_price_w_com = ? WHERE mnt_id = ?`,
-    [group, brand, model, size, hz, panel, resolution, price_srp, price, id], (err, result) => {
+    [group, brand, model, size, hz, panel, resolution, price_srp, price_w_com, id], (err, result) => {
       if(err) throw err
       res.send("Data updated successsfully")
     }
