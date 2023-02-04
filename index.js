1f765275c131553a397f722967877065f5205b63
theruk17 <therukza@hotmail.com>
2023-02-04 15:13:34 +0700

    update040223

--- index.js
+++ index.js
@@ -177,8 +177,7 @@ app.post('/upload_case', upload.single('file'), async (req, res) => {
     //connection.connect();
     rows.forEach(row => {
       connection.query(`INSERT INTO pd_test (case_id, case_group, case_brand, case_model, case_color, case_img, case_status, case_href, case_price_srp) 
-      VALUES ('${row[0]}', '${row[1]}', '${row[2]}', '${row[3]}', '${row[4]}', '${row[5]}', '${row[6]}', '${row[7]}', '${row[8]}') 
-      ON DUPLICATE KEY UPDATE case_id = '${row[0]}', case_group = '${row[1]}', case_brand = '${row[2]}', case_model = '${row[3]}', case_color = '${row[4]}', case_img = '${row[5]}', case_status = '${row[6]}', case_href = '${row[7]}', case_price_srp = '${row[8]}'`),
+      VALUES ('${row[0]}', '${row[1]}', '${row[2]}', '${row[3]}', '${row[4]}', '${row[5]}', '${row[6]}', '${row[7]}', '${row[8]}')`),
       (err, result) => {
         if (err) throw err;
         console.log(`Inserted ${result.affectedRows} row(s)`);
